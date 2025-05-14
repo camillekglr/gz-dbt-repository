52c54f9245bd70f91509de674765d7c80b95a666
camillekglr@gmail.com <camillekglr@gmail.com>
2025-05-14 20:44:20 +0000

    updated facebook staging model due to column name change in raw file

--- models/staging/raw/stg_raw__facebook.sql
+++ models/staging/raw/stg_raw__facebook.sql
@@ -10,7 +10,7 @@ renamed as (
 
     select
         date_date,
-        paid_source,
+        source_platform AS paid_source,
         campaign_key,
         campgn_name AS campaign_name,
         CAST(ads_cost AS FLOAT64) AS ads_cost,
